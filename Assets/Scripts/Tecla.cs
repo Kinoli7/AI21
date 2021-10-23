675bec5b9406c8676da7cfd4e634754c4350de46
Kinoli7 <kinoli@hotmail.com>
2021-10-23 14:52:01 +0200

    Gitignore updated and Alegria, GameLogic and Tecla scripts

---
+++ Assets/Scripts/Tecla.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tecla : MonoBehaviour
+{
+    public string Nota;
+    public string Respuesta;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
